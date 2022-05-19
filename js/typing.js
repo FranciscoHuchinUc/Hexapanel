48bcdc975d1688b21990dd22ccd025995801d02a
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-05-19 12:44:28 -0500

    Initial Commit

---
+++ js/typing.js
@@ -0,0 +1,13 @@
+
+let strings = [
+  `NATURALEZA QUE MEJORA: <strong>LA CALIDAD DE VIDA</strong>`,
+  `NATURALEZA QUE MEJORA: <strong>EL AMBIENTE</strong>`,
+  `NATURALEZA QUE MEJORA: <strong>A LA VIDA FORESTAL<strong>`];
+
+var typed3 = new Typed('#presentation', {
+  strings,
+  typeSpeed: 40,
+  backSpeed: 20,
+  smartBackspace: true,
+  loop: true
+});
